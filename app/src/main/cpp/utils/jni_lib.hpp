fecbb44085c08f3fd7c6359d7a18c6d5fadc4c4d
jia.li <ga402159806@gmail.com>
2019-01-14 14:09:14 +0800

    添加注释说明

--- app/src/main/cpp/utils/jni_lib.hpp
+++ app/src/main/cpp/utils/jni_lib.hpp
@@ -15,37 +15,57 @@ std::string jstring_to_string(JNIEnv *env, jstring j_str) {
 }
 
 std::vector<std::string> array_list_to_vector(JNIEnv *env, jobject arrayList) {
-    jclass java_util_ArrayList = static_cast<jclass>(env->NewGlobalRef(env->FindClass("java/util/ArrayList")));
+    //先找到要调用的类，ArrayList，这里使用全局引用，也可以使用局部引用
+    jclass java_util_ArrayList = static_cast<jclass> (env->NewGlobalRef(env->FindClass("java/util/ArrayList")));
+    // 获取java方法id
+    // 参数2是调用的方法名，<init>表示构造函数
+    // 参数3表示方法签名，(I)V表示参数为int型，返回值void型
     jmethodID java_util_ArrayList_size = env->GetMethodID(java_util_ArrayList, "size", "()I");
+    // 同上，参数2表示add方法，参数3表示参数为Object对象，返回值为boolean型
     jmethodID java_util_ArrayList_get = env->GetMethodID(java_util_ArrayList, "get", "(I)Ljava/lang/Object;");
-
+    // 调用size方法，获取arrayList的大小
     jint len = env->CallIntMethod(arrayList, java_util_ArrayList_size);
+    // 创建vector对象
     std::vector<std::string> result;
     result.reserve(len);
     for (jint i = 0; i < len; i++) {
+        // 调用get方法，获取jstring
         jstring element = static_cast<jstring>(env->CallObjectMethod(arrayList, java_util_ArrayList_get, i));
-        const char *pchars = env->GetStringUTFChars(element, nullptr);
-        result.emplace_back(pchars);
-        env->ReleaseStringUTFChars(element, pchars);
+        // jstring转char*
+        const char *p_chars = env->GetStringUTFChars(element, nullptr);
+        // 将char*添加到vector中
+        result.emplace_back(p_chars);
+        // 删除引用
+        env->ReleaseStringUTFChars(element, p_chars);
         env->DeleteLocalRef(element);
     }
+    // 删除全局引用
     env->DeleteGlobalRef(java_util_ArrayList);
     return result;
 }
 
 jobject vector_to_array_list(JNIEnv *env, std::vector<std::string> vector) {
+    //先找到要调用的类，ArrayList，这里使用全局引用，也可以使用局部引用
     jclass java_util_ArrayList = static_cast<jclass>(env->NewGlobalRef(env->FindClass("java/util/ArrayList")));
+    // 获取java方法id
+    // 参数2是调用的方法名，<init>表示构造函数
+    // 参数3表示方法签名，(I)V表示参数为int型，返回值void型
     jmethodID java_util_ArrayList_ = env->GetMethodID(java_util_ArrayList, "<init>", "(I)V");
+    // 同上，参数2表示add方法，参数3表示参数为Object对象，返回值为boolean型
     jmethodID java_util_ArrayList_add = env->GetMethodID(java_util_ArrayList, "add", "(Ljava/lang/Object;)Z");
-
+    // 调用构造函数创建ArrayList对象
     jobject result = env->NewObject(java_util_ArrayList, java_util_ArrayList_, vector.size());
+    // 遍历vector
     for (std::string s: vector) {
+        // string转jstring
         jstring element = env->NewStringUTF(s.c_str());
+        // 调用add方法，将jstring添加到arrayList中
         env->CallBooleanMethod(result, java_util_ArrayList_add, element);
+        // 删除jstring引用
         env->DeleteLocalRef(element);
     }
+    // 删除全局引用
     env->DeleteGlobalRef(java_util_ArrayList);
-
     return result;
 }
 
@@ -58,7 +78,7 @@ std::string callJavaStaticMethod(JNIEnv *env, std::string class_name,
         return nullptr;
     }
     // 获取java方法id
-    // 参数二是调用的方法名,  参数三是方法的签名
+    // 参数二是调用的方法名,参数三是方法的签名
     jmethodID id = env->GetStaticMethodID(clazz, method_name.c_str(), method_sign.c_str());
     if (id == nullptr) {
         LOGE("find method %s error !", method_name.c_str());
@@ -68,6 +88,6 @@ std::string callJavaStaticMethod(JNIEnv *env, std::string class_name,
     jstring result = (jstring) env->CallStaticObjectMethod(clazz, id); //调用java方法
     // 释放资源
     env->DeleteLocalRef(clazz);
-    // 转换jstring为string用于显示
+    // 将jstring转换为string
     return jstring_to_string(env, result);
 }
